417706285027ce7ead987df1079e51a802a368dc
agathesf <agathe.saintfrison@gmail.com>
2021-09-01 14:57:07 +0200

    push before heroku push

--- app/javascript/components/close.js
+++ app/javascript/components/close.js
@@ -2,13 +2,17 @@ const closeModal = () => {
     // var about = document.getElementById('about');
     // about.addEventListener("click", closeModal);
 
-    console.log("hello")
-    const about_modal = document.getElementById('aboutModalCenter');
-    // var about = document.getElementById('about');
-    console.log("caca")
-    if (about_modal.classList.contains("show")) {
-        about.innerHTML = about.innerHTML.replace("about","close");
-    };
+    setTimeout(() => {   
+
+        const about_modal = document.getElementById('aboutModalCenter');
+        // var about = document.getElementById('about');
+        console.log("caca")
+        if (about_modal.classList.contains("show")) {
+            about.innerHTML = about.innerHTML.replace("about","close");
+        };
+
+    }, 200);
+
 }
 
 export { closeModal }
