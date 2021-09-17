427ffb675f50c8ac0b603de6cba63de96cf72c17
agathesf <agathe.saintfrison@gmail.com>
2021-09-17 14:02:15 +0200

    debug

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,6 +2,7 @@ Cloudinary.config do |config|
   config.cloud_name = 'dowlbltsc'
   config.api_key = '515459721288771'
   config.api_secret = 'o5W8OFmNs816Pulwjr61yH2Q7Jw'
+  config.enhance_image_tag = true
   config.secure = true
   config.cdn_subdomain = true
 end
