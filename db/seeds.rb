273cd8f3b57c759c5fa9b17bc62a049c685c32ab
agathesf <agathe.saintfrison@gmail.com>
2021-09-17 14:22:15 +0200

    delete seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,47 +11,47 @@ require 'faker'
 Model.destroy_all
 User.destroy_all
 
-10.times do
+# 10.times do
 
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
-  description = "jeune influenceur de 20 ans. depuis de nombreuses années désormais, sa manière d’évoquer ses doutes, ses ambitions avec toujours beaucoup d’humour mais aussi une véritable poésie lui a permis de réunir une communauté fidèle et engagée."
-  age = rand(20..27)
-  gender = ["male", "female"].sample
-  type = ["Model", "Actor", "Rapper"].sample
-  category = ["new faces", "talents"].sample
-  based_in = Faker::Address.city
-  height = rand(169..201).to_s
-  chest = rand(80..95).to_s
-  waist = rand(57..80).to_s
-  hips = rand(80..100).to_s
-  foot = rand(37..43).to_s
-  hair = ["Brun", "Blond", "Roux"].sample
-  eye = ["Bleus", "Verts", "Marrons"].sample
-  email = Faker::Internet.email
-  instagram = Faker::Name.name  
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   description = "jeune influenceur de 20 ans. depuis de nombreuses années désormais, sa manière d’évoquer ses doutes, ses ambitions avec toujours beaucoup d’humour mais aussi une véritable poésie lui a permis de réunir une communauté fidèle et engagée."
+#   age = rand(20..27)
+#   gender = ["male", "female"].sample
+#   type = ["Model", "Actor", "Rapper"].sample
+#   category = ["new faces", "talents"].sample
+#   based_in = Faker::Address.city
+#   height = rand(169..201).to_s
+#   chest = rand(80..95).to_s
+#   waist = rand(57..80).to_s
+#   hips = rand(80..100).to_s
+#   foot = rand(37..43).to_s
+#   hair = ["Brun", "Blond", "Roux"].sample
+#   eye = ["Bleus", "Verts", "Marrons"].sample
+#   email = Faker::Internet.email
+#   instagram = Faker::Name.name  
 
-  model = Model.create!(first_name: first_name, 
-  						last_name: last_name, 
-  						description: description,
-  						age: age,
-  						gender: gender,
-  						role: type,
-  						category: category, 
-  						based_in: based_in,
-  						height: height,
-  						chest: chest,
-  						waist: waist,
-  						hips: hips,
-  						foot: foot,
-  						hair: hair,
-  						eye: eye,
-  						email: email,
-  						instagram: instagram
-  						)
+#   model = Model.create!(first_name: first_name, 
+#   						last_name: last_name, 
+#   						description: description,
+#   						age: age,
+#   						gender: gender,
+#   						role: type,
+#   						category: category, 
+#   						based_in: based_in,
+#   						height: height,
+#   						chest: chest,
+#   						waist: waist,
+#   						hips: hips,
+#   						foot: foot,
+#   						hair: hair,
+#   						eye: eye,
+#   						email: email,
+#   						instagram: instagram
+#   						)
 
 
-end
+# end
 
 User.create!(email: "test.123@example.com", password: "Test.123")
 
