a5b9ef10330bda4e9f1b022ed11d09489c8c6034
agathesf <agathe.saintfrison@gmail.com>
2021-09-17 14:07:58 +0200

    delete static_file_support: true

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -2,7 +2,6 @@ Cloudinary.config do |config|
   config.cloud_name = 'dowlbltsc'
   config.api_key = '515459721288771'
   config.api_secret = 'o5W8OFmNs816Pulwjr61yH2Q7Jw'
-  config.enhance_image_tag = true
   config.secure = true
   config.cdn_subdomain = true
 end
